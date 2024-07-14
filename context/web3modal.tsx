a9c7d7ab4fbd7e8bd6976b1790dcad6a12b3696f
Luke Foster <116062686+LukeFost@users.noreply.github.com>
2024-07-14 15:07:57 +0200

    (no commit message provided)

--- context/web3modal.tsx
+++ context/web3modal.tsx
@@ -1,7 +1,5 @@
 'use client'
 
-import { createWeb3Modal, defaultConfig } from '@web3modal/ethers5/react'
-
 // 1. Get projectId at https://cloud.walletconnect.com
 const projectId = 'NEXT_PUBLIC_PROJECT_ID'
 
@@ -14,11 +12,31 @@ const inco = {
   explorerUrl: ''
 }
 
+// 5. Create a Web3Modal instance
+createWeb3Modal({
+  ethersConfig,
+  chains: [inco],
+  projectId,
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+  enableOnramp: true // Optional - false as default
+})
+
+import { createWeb3Modal, defaultConfig } from '@web3modal/ethers5/react'
+
+// 2. Set chains
+const incoGentry = {
+  chainId: 9090,
+  name: 'Inco Gentry Testnet',
+  currency: 'INCO',
+  explorerUrl: 'https://testnet.incoscan.io',
+  rpcUrl: 'https://testnet.inco.org'
+}
+
 // 3. Create a metadata object
 const metadata = {
   name: 'My Website',
   description: 'My Website description',
-  url: '', // origin must match your domain & subdomain
+  url: 'https://mywebsite.com', // origin must match your domain & subdomain
   icons: ['https://avatars.mywebsite.com/']
 }
 
@@ -30,13 +48,15 @@ const ethersConfig = defaultConfig({
   /*Optional*/
   enableEIP6963: true, // true by default
   enableInjected: true, // true by default
-  enableCoinbase: false,
+  enableCoinbase: true, // true by default
+  rpcUrl: 'https://testnet.inco.org', // used for the Coinbase SDK
+  defaultChainId: 9090 // used for the Coinbase SDK
 })
 
 // 5. Create a Web3Modal instance
 createWeb3Modal({
   ethersConfig,
-  chains: [inco],
+  chains: [incoGentry],
   projectId,
   enableAnalytics: true, // Optional - defaults to your Cloud configuration
   enableOnramp: true // Optional - false as default
